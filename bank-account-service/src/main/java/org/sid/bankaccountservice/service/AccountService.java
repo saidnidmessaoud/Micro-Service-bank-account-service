13b5fa8e0aed0d8578719eb3612d9be43328fb0c
saidnidmessaoud <nidmessaoudsaid1@gmail.com>
2024-01-12 21:27:13 +0000

    V1

---
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/service/AccountService.java
@@ -0,0 +1,10 @@
+package org.sid.bankaccountservice.service;
+
+import org.sid.bankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.bankaccountservice.dto.BankAccountResponseDTO;
+
+public interface AccountService {
+    BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO);
+
+    BankAccountResponseDTO updateAccount(String id, BankAccountRequestDTO bankAccountDTO);
+}
